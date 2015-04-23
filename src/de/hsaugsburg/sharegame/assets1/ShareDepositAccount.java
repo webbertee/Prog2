727ff2ff1a5c29c53b8c3888c0b8fdb1690178e1
webbertee <p.s.weber@gmx.de>
2015-04-23 15:04:43 +0200

    -rename step 1

---
+++ src/de/hsaugsburg/sharegame/assets1/ShareDepositAccount.java
@@ -0,0 +1,155 @@
+package de.hsaugsburg.sharegame.assets;
+public class ShareDepositAccount extends Asset{
+
+	private int shareItemCount;
+	private ShareItem[] shareItems;
+//	private final int DEFAUT_LENGTH = 10;
+//	private final int LENGTH_CHANGE = 10;
+	
+	public int getLengthDebug() {
+		return shareItems.length;
+	}
+
+	public ShareDepositAccount(String name) {
+		super(name);
+		shareItemCount = 0;
+		
+		//shareItems = new ShareItem[DEFAUT_LENGTH];
+		//Alternativ
+		shareItems = new ShareItem[0];
+	}
+
+	public String toString() {
+		return "ShareDepositAccount with " + shareItemCount + " ShareItems";
+	}
+	
+	public int getShareCount(Share share) {
+		int saindex = findShareItem(share);
+		if(saindex < 0)
+			return 0;
+		else
+			return shareItems[saindex].getCount();
+	}
+	
+	public long getValue() {
+		long value = 0;
+		for (int i = 0; i < shareItemCount; i++) {
+			value += shareItems[i].getValue();
+		}
+		return value;
+	}
+	public long getBuyValue() {
+		long value = 0;
+		for (int i = 0; i < shareItemCount; i++) {
+			value += shareItems[i].getBuyValue();
+		}
+		return value;
+	}
+
+	/**
+	 * 
+	 * @param share
+	 * @param count
+	 */
+	public void addShare(Share share, int count) {
+		// suche Entsprechendes Paket im Array
+		int shareItemIndex = findShareItem(share);
+		if (shareItemIndex >= 0) {
+			shareItems[shareItemIndex].add(count);
+			return;
+		}
+
+		// Kein entsprechendes Paket vorhanden.
+		
+		checkAddSize();
+		shareItems[shareItemCount] = new ShareItem(share, super.getName() + "_" + share.getName());
+		shareItems[shareItemCount].add(count);
+		shareItemCount++;
+		
+	}
+
+	private int findShareItem(Share share) {
+		for (int i = 0; i < shareItemCount; i++) {
+			if (shareItems[i].getName().endsWith("_" + share.getName())) { 
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	private void checkAddSize() {
+//		if (shareItems.length <= shareItemCount) {
+//			ShareItem[] newShareItems = new ShareItem[shareItems.length
+//					+ LENGTH_CHANGE];
+//			System.arraycopy(shareItems, 0, newShareItems, 0, shareItemCount);
+//			shareItems = newShareItems;
+//		}
+		
+		//Alternativ
+		ShareItem[] newShareItems = new ShareItem[shareItems.length + 1];
+		System.arraycopy(shareItems, 0, newShareItems, 0, shareItemCount);
+		shareItems = newShareItems;
+	}
+	
+	private void checkRemoveSize() {
+//		if(shareItems.length > shareItemCount + LENGTH_CHANGE) {
+//			ShareItem[] newShareItems = new ShareItem[shareItemCount 
+//			                                          + LENGTH_CHANGE / 2]; //shareItemCount + x?
+//			System.arraycopy(shareItems, 0, newShareItems, 0, shareItemCount);
+//			shareItems = newShareItems;
+//		}
+		
+		//Alternativ
+		ShareItem[] newShareItems = new ShareItem[shareItems.length - 1];
+		System.arraycopy(shareItems, 0, newShareItems, 0, shareItemCount);
+		shareItems = newShareItems;
+	}
+
+	/**
+	 * 
+	 * @param share
+	 * @param count
+	 */
+	public void removeShare(Share share, int count) {
+		int shareItemIndex = findShareItem(share);
+		// abbrechen, wenn das shareItem nicht vorhanden ist
+		// oder das hinzufügen scheitert
+		if (shareItemIndex < 0)
+			throw new IllegalStateException("No " + share.getName() + " shares in " + this.toString());
+		
+		shareItems[shareItemIndex].remove(count);
+		
+		//Wenn das Depot leer ist, entferne es aus dem Speicher
+		if (shareItems[shareItemIndex].getCount() == 0) {
+			System.arraycopy(shareItems, shareItemIndex + 1, shareItems,
+					shareItemIndex, shareItemCount - shareItemIndex - 1);
+			shareItemCount--;
+			checkRemoveSize();
+		}
+	}
+
+	public int getSharesCount(Share share) {
+		int i = findShareItem(share);
+		if(i < 0)
+			return 0;
+		
+		return shareItems[i].getCount();
+		
+	}
+
+	public long getSharesValue(Share share) {
+		int i = findShareItem(share);
+		if(i < 0)
+			return 0;
+		
+		return shareItems[i].getValue();
+	}
+
+	public long getSharesBuyValue(Share share) {
+		int i = findShareItem(share);
+		if(i < 0)
+			return 0;
+		
+		return shareItems[i].getBuyValue();
+	}
+}
